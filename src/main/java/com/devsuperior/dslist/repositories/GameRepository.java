3dee45984937cee342285496f93b6ff412a44219
Leticia Galvao <legalvao1@gmail.com>
2025-08-19 22:03:10 -0300

    Database seed update

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -3,7 +3,7 @@ package com.devsuperior.dslist.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dslist.entities.Game;
-
+//O Jpa respositorie só permite uma chave primaria do tipo Long
 public interface GameRepository extends JpaRepository<Game, Long>{
     
 }
