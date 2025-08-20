9a07bcc806301928fdd272cd53f659dbcfdca30b
Leticia Galvao <legalvao1@gmail.com>
2025-08-19 22:25:02 -0300

    Get game by ID

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -1,10 +1,13 @@
 package com.devsuperior.dslist.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
@@ -14,10 +17,19 @@ public class GameService {
     
     @Autowired // injeção de dependencia
     private GameRepository gameReposity;
-
+    
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameReposity.findAll();
         return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
 
+    @Transactional(readOnly = true) //não bloqueia o banco para escrita, deixando mais rápido
+    //boa pratica para que o service fique transacional, 
+    //garantindo que as operações no banco vão acontecer
+    //obedecendo o principio acid (atomica, consistente, isolada e duravel)
+    public GameDTO findById(Long id){
+        Game result = gameReposity.findById(id).get();
+        return new GameDTO(result);
     }
 }
